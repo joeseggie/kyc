38e45bea768b60a4b5a2833a5b55432695e00fac
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-07-05 15:41:10 +0300

    Subscriber model

---
+++ UgandaTelecom.Kyc/UgandaTelecom.Kyc.Core/Models/Subscriber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UgandaTelecom.Kyc.Core.Models
+{
+    public class Subscriber
+    {
+        public Guid SubscriberId { get; set; }
+        public string Surname { get; set; }
+        public string GivenName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string IdentificationNumber { get; set; }
+        public string Msisdn { get; set; }
+        public string IdentificationType { get; set; }
+        public string Village { get; set; }
+        public string District { get; set; }
+        public string FaceImg { get; set; }
+        public string IdFrontimg { get; set; }
+        public string IdBackimg { get; set; }
+        public string AgentMsisdn { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public TimeSpan RegistrationTime { get; set; }
+        public string Mode { get; set; }
+        public bool Verified { get; set; }
+        public string VerificationRequest { get; set; }
+        public string NiraValidation { get; set; }
+        public string OtherNames { get; set; }
+        public string IdCardNumber { get; set; }
+        public DateTime VisaExpiry { get; set; }
+    }
+}
