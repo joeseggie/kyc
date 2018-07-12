dd6db5285378d7141d1e38d121e9c64d2150636a
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-07-12 14:24:05 +0300

    Getting Msente configurations.

---
+++ UgandaTelecom.Kyc/UgandaTelecom.Kyc.Core/Data/MsenteApiConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UgandaTelecom.Kyc.Core.Data
+{
+    public class MsenteApiConfiguration
+    {
+        public virtual string Url { get; set; }
+        public virtual string HttpMethod { get; set; }
+    }
+}
---
+++ UgandaTelecom.Kyc/UgandaTelecom.Kyc.Core/Data/MsenteAppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UgandaTelecom.Kyc.Core.Data
+{
+    public class MsenteAppSettings
+    {
+        public virtual MsenteApiConfiguration CredentialsApi { get; set; }
+        public virtual MsenteApiConfiguration RegisterApi { get; set; }
+        public virtual MsenteApiConfiguration ValidateNumberApi { get; set; }
+    }
+}
