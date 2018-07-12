b8746ce54eb77c2c98027d569f3c7a3a4f05f5f4
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-07-12 14:24:40 +0300

    Holds the response from MSente token result operation.

---
+++ UgandaTelecom.Kyc/UgandaTelecom.Kyc.Core/Common/OperationResults/MsenteTokenRequestResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UgandaTelecom.Kyc.Core.Common.OperationResults
+{
+    public class MsenteTokenRequestResult
+    {
+        public virtual string ReferenceId { get; set; }
+        public virtual object ResponseResult { get; set; }
+    }
+}
