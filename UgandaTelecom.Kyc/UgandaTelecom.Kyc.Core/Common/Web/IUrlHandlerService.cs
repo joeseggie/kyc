6d281db8ed0811a1b8cc767eb96c090325786549
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-07-12 14:22:34 +0300

    Service to handle making web requests

---
+++ UgandaTelecom.Kyc/UgandaTelecom.Kyc.Core/Common/Web/IUrlHandlerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UgandaTelecom.Kyc.Core.Services
+{
+    public interface IUrlHandlerService
+    {
+        Task<string> ProcessPOSTRequestAsync(string url, string body, string bearerToken);
+        Task<string> ProcessPOSTRequestAsync(string url, string body);
+        Task<string> ProcessGETRequestAsync();
+    }
+}
---
+++ UgandaTelecom.Kyc/UgandaTelecom.Kyc.Core/Common/Web/UrlHandlerService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UgandaTelecom.Kyc.Core.Services
+{
+    public class UrlHandlerService : IUrlHandlerService
+    {
+        public Task<string> ProcessGETRequestAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> ProcessPOSTRequestAsync(string url, string body, string bearerToken)
+        {
+            using (var client = new HttpClient())
+            {
+                client.MaxResponseContentBufferSize = 256000;
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+                var uri = new Uri(url);
+                var content = new StringContent(body, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = null;
+                response = await client.PostAsync(uri, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+
+                throw new ApplicationException($"HttpPost request to {url} with body {body} failed with HttpStatusCode {response.StatusCode} content: {(await response.Content.ReadAsStringAsync())}");
+            }
+        }
+
+        public async Task<string> ProcessPOSTRequestAsync(string url, string body)
+        {
+            using (var client = new HttpClient())
+            {
+                client.MaxResponseContentBufferSize = 256000;
+                var uri = new Uri(url);
+                var content = new StringContent(body, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = null;
+                response = await client.PostAsync(uri, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return responseContent;
+                }
+
+                throw new ApplicationException($"HttpPost request to {uri} with body {body} failed with HttpStatusCode {response.StatusCode} content: {(await response.Content.ReadAsStringAsync())}");
+            }
+        }
+    }
+}
