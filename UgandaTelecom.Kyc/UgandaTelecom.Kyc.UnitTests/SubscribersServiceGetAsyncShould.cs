f2f939bdf480d45a3ff618358273fbc8d063de5d
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-07-13 09:45:46 +0300

    UnitTest to assert getting the database server connection for the task to get subscriber details.

---
+++ UgandaTelecom.Kyc/UgandaTelecom.Kyc.UnitTests/SubscribersServiceGetAsyncShould.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using UgandaTelecom.Kyc.Core.Data;
+using UgandaTelecom.Kyc.Core.Services.Subscribers;
+using Xunit;
+
+namespace UgandaTelecom.Kyc.UnitTests
+{
+    public class SubscribersServiceGetAsyncShould
+    {
+        [Fact, Trait("Service", "SubscriberService"), Trait("Feature", "GetSubscriber")]
+        public async Task CallSqlDatabaseServerConnectionProperty()
+        {
+            // Given
+            var mockSqlDatabaseServer = new Mock<ISqlDatabaseServer>();
+            mockSqlDatabaseServer.Setup(m => m.Connection).Returns(new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SimRegistration;Integrated Security=True;MultipleActiveResultSets=true"));
+            var testService = new SubscriberService(mockSqlDatabaseServer.Object);
+            var mockMsisdn = "711187734";
+
+            // When
+            await testService.GetAsync(mockMsisdn);
+
+            // Then
+            mockSqlDatabaseServer.Verify(m => m.Connection, Times.AtMostOnce());
+        }
+    }
+}
