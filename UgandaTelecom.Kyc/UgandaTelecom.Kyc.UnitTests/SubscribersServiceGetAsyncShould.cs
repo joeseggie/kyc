9bdfc62040453c964dd2d64f5fb53ffdcd321875
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-07-13 10:02:39 +0300

    UnitTest Subscriber GetAsync method to return Subscriber object.

--- UgandaTelecom.Kyc/UgandaTelecom.Kyc.UnitTests/SubscribersServiceGetAsyncShould.cs
+++ UgandaTelecom.Kyc/UgandaTelecom.Kyc.UnitTests/SubscribersServiceGetAsyncShould.cs
@@ -1,10 +1,12 @@
 ﻿using Moq;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 using UgandaTelecom.Kyc.Core.Data;
+using UgandaTelecom.Kyc.Core.Models;
 using UgandaTelecom.Kyc.Core.Services.Subscribers;
 using Xunit;
 
@@ -26,6 +28,22 @@ namespace UgandaTelecom.Kyc.UnitTests
 
             // Then
             mockSqlDatabaseServer.Verify(m => m.Connection, Times.AtMostOnce());
+        }
+
+        [Fact, Trait("Service", "SubscriberService"), Trait("Feature", "GetSubscriber")]
+        public async Task ReturnsSubscriberObject()
+        {
+            // Given
+            var mockSqlDatabaseServer = new Mock<ISqlDatabaseServer>();
+            mockSqlDatabaseServer.Setup(m => m.Connection).Returns(new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SimRegistration;Integrated Security=True;MultipleActiveResultSets=true"));
+            var testService = new SubscriberService(mockSqlDatabaseServer.Object);
+            var mockMsisdn = "711187734";
+
+            // When
+            var result = await testService.GetAsync(mockMsisdn);
+
+            // Then
+            result.ShouldBeOfType<Subscriber>();
         }
     }
 }
