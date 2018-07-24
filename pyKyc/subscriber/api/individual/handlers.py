2a61efceaf033eda1231750c6d30d3a4acecdd63
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-07-24 12:30:57 +0300

    Handler for individual subscriber operations

---
+++ pyKyc/subscriber/api/individual/handlers.py
@@ -0,0 +1,41 @@
+"""Individual subscriber tasks handler
+"""
+
+import pyodbc
+from subscriber import app
+
+class IndividualSubscriberHandler():
+    """Object to handle all operations for individual subscribers
+    """
+
+    def __init__(self):
+        """Constructor
+        """
+        self.cnxn = self.db_connection()
+    
+    def db_connection(self):
+        """Gets connection to the database
+
+        Returns: pyodbc connect object
+        """
+        return pyodbc.connect('DRIVER={ODBC DRIVER 17 for SQL Server}' + 
+        ';SERVER=' + app.config['DATABASE_CONFIG']['HOST'] + 
+        ';DATABASE=' + app.config['DATABASE_CONFIG']['DB'] +
+        ';UID=' + app.config['DATABASE_CONFIG']['USER'] + 
+        ';PWD=' + app.config['DATABASE_CONFIG']['PASSWORD'])
+
+    def register_subscriber(self, subscriber):
+        """Registers subscriber
+        
+        Arguments:
+            subscriber {obj} -- Subscriber details for registration
+        """
+        with self.cnxn as connection:
+            cursor = connection.cursor()
+            cursor = cursor.execute('SELECT @@version;')
+            row = cursor.fetchone()
+
+            while row:
+                sqlserver_version = row[0]
+                row = cursor.fetchone()
+        return sqlserver_version
